d7c37816d3b9610b90ba8922b4a15c6746b7f7bb
cseas <contact.abhijeetsingh@gmail.com>
2021-04-03 16:13:48 +0530

    Add state management & API fetch example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+
+const API_BASE = "https://randomuser.me/api";
 
 export function App() {
   const [count, setCount] = useState(0);
-  return <></>;
+  const [data, setData] = useState([]);
+
+  function getUser() {
+    fetch(API_BASE)
+      .then((response) => response.json())
+      .then((data) => setData(data.results))
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+
+      <button onClick={() => getUser()}>Get User</button>
+
+      {data.length === 0 ? (
+        <p>Loading</p>
+      ) : (
+        <pre>{JSON.stringify(data[0]?.name, null, 2)}</pre>
+      )}
+    </>
+  );
 }
